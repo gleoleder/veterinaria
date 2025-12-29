65c6eabfe33c17f6f8c0eca78230e261fbf61b7e
gleoleder <gleo90000@gmail.com>
2025-12-29 01:35:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     API_KEY: 'AIzaSyAOhGTjJXHhuUhqf1g2DPCla59xNzftb-Q',
     
     // Carpeta de Drive para archivos (opcional - dejar vacío si no se usa)
-    DRIVE_FOLDER_ID: '',
+    DRIVE_FOLDER_ID: '1jw8mhMwtRwkHU3RIPasdcc3O_7-rnesH',
     
     // Nombres de las hojas en Google Sheets
     // IMPORTANTE: Deben coincidir EXACTAMENTE con los nombres de las pestañas
